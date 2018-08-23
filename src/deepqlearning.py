f571754155f77eaea35790058eeecd116c7f1919
Wencan Luo <wencanluo.cn@gmail.com>
2018-08-23 20:49:18 +0000

    add deepq

---
+++ src/deepqlearning.py
@@ -0,0 +1,14 @@
+import tensorflow as tf      # Deep Learning library
+import numpy as np           # Handle matrices
+from vizdoom import *        # Doom Environment
+
+import random                # Handling random number generation
+import time                  # Handling time calculation
+from skimage import transform# Help us to preprocess the frames
+
+from collections import deque# Ordered collection with ends
+import matplotlib.pyplot as plt # Display graphs
+
+import warnings # This ignore all the warning messages that are normally printed during the training because of skiimage
+warnings.filterwarnings('ignore')
+
